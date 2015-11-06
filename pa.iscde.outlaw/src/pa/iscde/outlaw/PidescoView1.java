dd8f57bc1b65960aadae11c4eeb4c8dd16b56f1c
JorgeBranquinho <jmabo@iscte.pt>
2015-11-06 17:35:19 +0000

    versão 2

--- pa.iscde.outlaw/src/pa/iscde/outlaw/PidescoView1.java
+++ pa.iscde.outlaw/src/pa/iscde/outlaw/PidescoView1.java
@@ -1,14 +1,15 @@
 package pa.iscde.outlaw;
 
+import java.awt.Color;
 import java.io.File;
 import java.util.Map;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-
 import pt.iscte.pidesco.extensibility.PidescoView;
 import pt.iscte.pidesco.javaeditor.internal.JavaEditorActivator;
 import pt.iscte.pidesco.javaeditor.service.JavaEditorServices;
@@ -21,19 +22,26 @@ public class PidescoView1 implements PidescoView {
 
 	@Override
 	public void createContents(Composite viewArea, Map<String, Image> imageMap) {
-		viewArea.setLayout(new RowLayout(SWT.HORIZONTAL));
+		viewArea.setLayout(new RowLayout(SWT.VERTICAL));
 		Label label = new Label(viewArea, SWT.NONE);
 		label.setImage(imageMap.get("smiley.png"));
-		Text text = new Text(viewArea, SWT.WRAP);
-		text.setEditable(false);
-		text.setBounds(0, 0, 0, 0);
-		
+		Text txtpackage = new Text(viewArea, SWT.WRAP);
+		Text txtclass = new Text(viewArea, SWT.WRAP);
+		txtclass.setEditable(false);
+		txtclass.setCursor(Display.getCurrent().getSystemCursor(SWT.CURSOR_ARROW));
+		txtpackage.setEditable(false);
+		txtpackage.setCursor(Display.getCurrent().getSystemCursor(SWT.CURSOR_ARROW));
 		
 		JavaEditorServices services = JavaEditorActivator.getInstance().getServices();
 		File f = services.getOpenedFile();
-		text.setText(f.getName());
+		txtclass.setText(f.getName());
+		txtpackage.setText(getPackage(f.getParent()));
 		
-		
+	}
+
+	private String getPackage(String parent) {
+		String[] path=parent.split("\\\\");
+		return path[path.length-1];
 	}
 
 }
