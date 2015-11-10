d554ddcbf92ddfe5cd14c534e55e87bc437b3822
JorgeBranquinho <jmabo@iscte.pt>
2015-11-10 10:46:42 +0000

    testar composites

--- pa.iscde.outlaw/src/pa/iscde/outlaw/PidescoView1.java
+++ pa.iscde.outlaw/src/pa/iscde/outlaw/PidescoView1.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.util.Map;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
@@ -25,9 +26,12 @@ public class PidescoView1 implements PidescoView {
 	public void createContents(Composite viewArea, Map<String, Image> imageMap) {
 		viewArea.setLayout(new RowLayout(SWT.VERTICAL));
 		
-		Label label = new Label(viewArea, SWT.NONE);
+		Composite composite = new Composite(viewArea, SWT.NONE);
+	    composite.setLayout(new GridLayout(2, true));
+		
+		Label label = new Label(composite, SWT.NONE);
 		label.setImage(imageMap.get("smiley.png"));
-		Text txtpackage = new Text(viewArea, SWT.WRAP);
+		Text txtpackage = new Text(composite, SWT.WRAP);
 		Text txtclass = new Text(viewArea, SWT.WRAP);
 		txtclass.setEditable(false);
 		txtclass.setCursor(Display.getCurrent().getSystemCursor(SWT.CURSOR_ARROW));
@@ -37,7 +41,6 @@ public class PidescoView1 implements PidescoView {
 		File f = services.getOpenedFile();
 		txtclass.setText(f.getName());
 		txtpackage.setText(getPackage(f.getParent()));
-		txtclass.setText("teste");
 		
 	}
 
