d6a1806dc590506ce02bbe66b92ab03e011dd006
Mr.Ivo <Mr.Ivo@cenas>
2015-11-16 20:22:44 +0000

    root added

---
+++ pa.iscde.outlaw/src/pa/iscde/outlaw/jorge/OutlineRoot.java
@@ -0,0 +1,15 @@
+package pa.iscde.outlaw.jorge;
+
+public class OutlineRoot {
+
+	private OutlineClass clazz;
+
+	public OutlineClass getClazz() {
+		return clazz;
+	}
+
+	public void setClazz(OutlineClass clazz) {
+		this.clazz = clazz;
+	}
+	
+}
